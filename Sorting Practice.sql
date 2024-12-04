fe5000189e425521db564d498bded01bffbf11d2
Beatriz Vela <70249199+betyvelavi@users.noreply.github.com>
2024-12-04 16:31:17 -0600

    Update Sorting Practice.sql

--- Sorting Practice.sql
+++ Sorting Practice.sql
@@ -1,7 +1,10 @@
-/* This is a practice on sorting using Google's BigQuery on a dataset containing movie data.*/
 SELECT * 
 FROM
   `eloquent-victor-405022.movie_data.movies`
--- ORDER BY should be the last clause in the query. It shows ascending order in automatic. However, can type DESC to sort by descending order. 
+-- To  filter by Comedy Genre, we'll use the WHERE clause. To add more conditions such as filtering by revenue, we will use the clause AND 
+WHERE
+  Genre =  'Comedy'
+  AND Revenue > 300000000
+-- ORDER BY should be the last clause in the query. It shows ascending order in automatic. However, can type DESC to sort by descending order.
 ORDER BY 
   `Release Date` DESC
